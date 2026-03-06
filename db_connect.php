2bec5d9391388c7559a69895dfa3abc06663226f
King Melvin Regacho <111517660+Kingmelvs@users.noreply.github.com>
2026-03-06 21:17:52 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') exit;
+
+// Credentials mula sa iyong screenshot
 $host = "interchange.proxy.rlwy.net";
 $port = "18246";
 $user = "root";
@@ -12,6 +15,6 @@ $dbname = "railway";
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
-    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
+    die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
 }
 ?>
